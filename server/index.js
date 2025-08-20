02467edb83f9fae7e951f2f096e1b2f8f43a4c25
Ameena.G <ameenag7781@gmail.com>
2025-08-20 18:59:29 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:5173';
+const ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:port_number';
 
 app.use(helmet());
 app.use(cors({ origin: ORIGIN }));
